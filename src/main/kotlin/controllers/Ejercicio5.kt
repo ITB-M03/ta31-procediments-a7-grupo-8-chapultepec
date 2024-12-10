87c35dc9fc817bf9ca172b2b0a954e65597d5495
angel.sardinha.7e8 <angel.sardinha.7e8@itb.cat>
2024-12-10 12:59:12 +0100

    Update

--- src/main/kotlin/controllers/Ejercicio5.kt
+++ src/main/kotlin/controllers/Ejercicio5.kt
@@ -8,11 +8,13 @@ import java.util.Scanner
 fun menu(scan : Scanner) : Int {
     var opciones : Int
 
-    print("Buenas tardes. \n" +
+    print("--------------------------------------------- \n" +
+            "Buenas tardes. \n" +
             "Opcion 1: Introducir un numero (push) \n" +
             "Opcion 2: Eliminar un numero (pop) \n" +
             "Opcion 3: Mostrar contenido de la pila \n" +
-            "Opcion 4: Salir del programa \n")
+            "Opcion 4: Salir del programa \n" +
+            "-------------------------------------------- \n")
 
     opciones = pedirNumerito("Me gustaria que me dijeses el numero de tu opcion: ", scan)
 
@@ -22,12 +24,29 @@ fun menu(scan : Scanner) : Int {
 /**
  *
  */
-fun bucle(scan : Scanner) {
+fun bucle(scan : Scanner, pila: MutableList<Int>) {
     var opciones = 0
     val cuatro = 4
+    val uno = 1
+    val dos = 2
+    val tres = 3
+    val white = "\u001b[0m"
+    val green = "\u001b[32m"
 
     while(opciones != cuatro){
         opciones = menu(scan)
+        if(opciones == 1){
+            introducirUnNumero(scan, pila)
+        }
+        else if(opciones == 2){
+            eliminarNumero(pila)
+        }
+        else if(opciones == 3){
+            mostrarContenido(pila)
+        }
+        else if(opciones == 4){
+            mostrarMensajeSinSalto( green + "Gracias por probar la app" + white)
+        }
     }
 }
 
@@ -45,9 +64,16 @@ fun definirPila() : MutableList<Int> {
  */
 fun introducirUnNumero(scan : Scanner, pila : MutableList<Int>) : MutableList<Int> {
     var numero : Int
+    val diez = 10
+    val red = "\u001b[31m"
+    val white = "\u001b[0m"
+
     numero = pedirNumerito("Digame el numero que quiere introducir: ", scan)
 
-    pila.add(numero)
+    if(pila.size <= diez){
+        pila.add(numero)
+    }
+    else printlnMSG(red + "NO, eso no se puede, la pila esta llena" + white)
 
     return pila
 }
@@ -56,6 +82,16 @@ fun introducirUnNumero(scan : Scanner, pila : MutableList<Int>) : MutableList<In
  *
  */
 fun eliminarNumero(pila : MutableList<Int>) : MutableList<Int> {
+    val red = "\u001b[31m"
+    val white = "\u001b[0m"
+    mostrarUltimo(pila)
+
+    try {
+        pila.removeLast()
+    }
+    catch ( e : NoSuchElementException){
+        printlnMSG(red + "Esto no se puede hacer, la pila esta vacia :(" + white)
+    }
 
     return pila
 }
@@ -63,9 +99,29 @@ fun eliminarNumero(pila : MutableList<Int>) : MutableList<Int> {
 /**
  *
  */
-fun mostrarUltimo(pila: MutableList<Int>, msg : String) : Int {
+fun mostrarUltimo(pila: MutableList<Int>){
     var ultimo : Int
     ultimo = pila.last()
 
-    return ultimo
+    mostrarMensajeSinSalto("El numero que has eliminado es: $ultimo \n")
+}
+
+/**
+ *
+ */
+fun mostrarContenido(pila: MutableList<Int>) {
+    mostrarMensajeSinSalto("Este es el contenido de la pila actualmente: $pila \n" )
+}
+
+/**
+ *
+ */
+fun main() {
+    var scan = abrirScanner()
+
+    var pila = definirPila()
+
+    bucle(scan, pila)
+
+    cerrarScanner(scan)
 }
