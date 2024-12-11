b030c737f1fa7817922411c7c1ff98100ce98774
angel.sardinha.7e8 <angel.sardinha.7e8@itb.cat>
2024-12-11 08:54:22 +0100

    Update_V3

--- src/main/kotlin/controllers/Ejercicio 1.kt
+++ src/main/kotlin/controllers/Ejercicio 1.kt
@@ -1,5 +1,6 @@
 package controllers
 import utilities.*
+import java.util.*
 
 /**
  * Funcion de inicio de codigo Iva
@@ -8,3 +9,78 @@ import utilities.*
 fun main(){
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+fun menu1(scan : Scanner) : Int {
+    var opcion : Int
+
+    mostrarMensajeSinSalto("""
+        ----------------------------------------
+                  Buenas tardes :3
+        Opcion 1: 1986
+        Opcion 2: 1992
+        Opcion 3: 1993
+        Opcion 4: 1995
+        Opcion 5: 2010
+        Opcion 6: 2012 (Antes del 15 de Julio)
+        Opcion 7: 2012 (Despues del 15 de Julio)
+        Opcion 8: Salir
+        -----------------------------------------
+    """.trimIndent())
+    opcion = pedirNumerito("Buenas, me gustaria que me dijeses de que año quieres aplicar el IVA, Introduce el número de tu opcion: ", scan)
+
+    return opcion
+}
+
+fun menu2(scan : Scanner, )
